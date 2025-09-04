80b840eb7b4913d349b7388b278ed070d21fc232
JunyoungCho07 <chojunyoung070523@gmail.com>
2025-09-05 08:46:07 +0900

    modified:   continued_training.py

--- continued_training.py
+++ continued_training.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         data='C:/Users/cho-j/OneDrive/바탕 화면/data2nd/data.yaml',
         epochs=20,              # 추가로 학습할 epoch 수 (기존 학습과 합쳐지지 않음!)
         batch=8,
-        imgsz=256,
+        imgsz=1024,
         lr0=1e-4,               # 보통 추가 학습 시 learning rate 낮추는 게 좋다
         device=0,
         project='C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_2025/runs',
